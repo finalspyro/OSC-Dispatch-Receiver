7f927b83875a2131abd3ac201eec315804a9561f
Sloop <66267333+spallis93@users.noreply.github.com>
2022-11-25 15:02:24 -0500

    fix

--- OSC-Receiver.py
+++ OSC-Receiver.py
@@ -1,13 +1,19 @@
-import argparse
-import math
-
 from pythonosc import dispatcher
 from pythonosc import osc_server
+from pythonosc import udp_client
+from typing import List, Any
 
-dispatcher = dispatcher.Dispatcher()
-dispatcher.map("/filter", print)
 
-server = osc_server.ThreadingOSCUDPServer(
-    ("127.0.0.1", 57130), dispatcher)
-print("Serving on {}".format(server.server_address))
-server.serve_forever()
+def main(address: str, *osc_arguments):
+    msg = osc_arguments[0]
+    print(osc_arguments[0])
+
+def sc_receiver(ip,port,path):
+    disp = dispatcher.Dispatcher()
+    handler = disp.map(path, main)
+    server = osc_server.ThreadingOSCUDPServer((ip,port), disp)
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
+
+if __name__ == '__main__':
+    sc_receiver("127.0.0.1", 57130, "/filter")
